e7010f09df5ace737417584548037c1a85954d9c
Ondrej Janacek <janacek.o@gmail.com>
2014-12-12 08:12:07 +0100

    Removes hardcoded test scenarios

--- BplusTreeApp/Program.cs
+++ BplusTreeApp/Program.cs
@@ -20,41 +20,5 @@ namespace BplusTreeApp
             
             File.WriteAllText("output.txt", tree.Dump());
         }
-
-        // test sets
-
-        /*static void Main(string[] args)
-        {
-            var tree = new BplusTree(3);
-            tree.Insert(1, "one");
-            tree.Insert(10, "ten");
-            tree.Insert(5, "five");
-            tree.Insert(3, "three");
-            tree.Insert(8, "eight");
-            tree.Insert(12, "twelve");
-            tree.Insert(7, "seven");
-            tree.Insert(9, "nine");
-            tree.Insert(20, "twenty");
-            tree.Insert(15, "5teen");
-            tree.Insert(18, "8teen");
-            tree.Insert(25, "twenty5");
-            Console.WriteLine(tree.Dump());
-        }*/
-
-        /*static void Main(string[] args)
-        {
-            var tree = new BplusTree(4);
-            tree.Insert(1, "one");
-            tree.Insert(10, "ten");
-            tree.Insert(5, "five");
-            tree.Insert(3, "three");
-            tree.Insert(8, "eight");
-            tree.Insert(12, "twelve");
-            tree.Insert(15, "5teen");
-            tree.Insert(20, "twenty");
-            tree.Insert(25, "twenty5");
-            tree.Insert(30, "thirty");
-            Console.WriteLine(tree.Dump());
-        }*/
     }
 }
